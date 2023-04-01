f1efd969d9f3269d7a08220bdc539e00243ad25c
Humzah Kiani <humzahkiani@Humzahs-MBP.local>
2023-04-01 19:16:30 -0400

    vite + vue3 + tailwindcss repo creation

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+</script>
+
+<template>
+  <!-- HEADER -->
+  <div class="container mx-auto">
+    <h1>
+    Autonomous Vehicles... Soon to Come or Wishful Thinking
+    </h1>
+  </div>
+
+  <!-- HEADER -->
+    
+  <!-- COUNTDOWN -->
+  <!-- COUNTDOWN -->
+
+  <!-- BET DETAILS -->
+  <div>
+    <h2>
+      THE BET
+    </h2>
+    <p>
+      Autonomous cars will be readily available for booking in an uber/taxi-like fashion in Richmond, VA by April 1, 2027
+    </p>
+    <p>
+      FOR: Peter Stercx
+      <br>AGAINST: Humzah Kiani
+    </p>
+  </div>
+</template>
+
+<style scoped>
+</style>
