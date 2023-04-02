ea4a64a791ebe71dd7f2796fe86e697377fb9c86
Humzah Kiani <humzahkiani@Humzahs-MBP.local>
2023-04-02 19:35:59 -0400

    mobile viewport modifications

--- src/App.vue
+++ src/App.vue
@@ -5,23 +5,23 @@
 <template>
   <!-- PAGE -->
     <!-- HEADER -->
-    <header class="flex flex-row pt-16 pb-16 ">
+    <header class="flex-row my-8">
       <h1 class="flex">
       Autonomous Vehicles... Soon to Come or Wishful Thinking?
       </h1>
     </header>
     <!-- HEADER -->
     
-    <div class = "flex-row pb-8">
+    <div class = "flex-row my-12">
       <vue3-flip-countdown mainColor = "b53f3f" labelColor = "cornsilk"  deadline="2027-4-1 00:00:00" deadlineDate="2027-04-01"/>
     </div>
 
     <!-- BET DETAILS -->
-    <div class = "flex-row pt-8 pb-8">
+    <div class = "flex-row mt-4">
       <h2>
         THE BET
       </h2>
-      <p class="pt-4">
+      <p class="mt-4 mx-4">
         Autonomous cars will be readily available for booking in an uber/taxi-like fashion in <span>Richmond, VA</span> by
         <span>
         April 1, 2027
