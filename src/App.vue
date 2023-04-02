5aada130f6608f08289fadf9d223a66449f87985
Humzah Kiani <humzahkiani@Humzahs-MBP.local>
2023-04-02 16:15:08 -0400

    basic outline of website

--- src/App.vue
+++ src/App.vue
@@ -3,31 +3,34 @@
 </script>
 
 <template>
-  <!-- HEADER -->
-  <div class="container mx-auto">
-    <h1>
-    Autonomous Vehicles... Soon to Come or Wishful Thinking
-    </h1>
-  </div>
-
-  <!-- HEADER -->
+  <!-- PAGE -->
+    <!-- HEADER -->
+    <header class="flex flex-row pt-16 pb-16">
+      <h1 class="flex">
+      Autonomous Vehicles... Soon to Come or Wishful Thinking?
+      </h1>
+    </header>
+    <!-- HEADER -->
     
-  <!-- COUNTDOWN -->
-  <!-- COUNTDOWN -->
 
-  <!-- BET DETAILS -->
-  <div>
-    <h2>
-      THE BET
-    </h2>
-    <p>
-      Autonomous cars will be readily available for booking in an uber/taxi-like fashion in Richmond, VA by April 1, 2027
-    </p>
-    <p>
-      FOR: Peter Stercx
-      <br>AGAINST: Humzah Kiani
-    </p>
-  </div>
+    <!-- BET DETAILS -->
+    <div class = "flex-row pt-8 pb-8">
+      <h2>
+        THE BET
+      </h2>
+      <p class="pt-4">
+        Autonomous cars will be readily available for booking in an uber/taxi-like fashion in Richmond, VA by
+        <span class="underline font-bold">
+        April 1, 2027
+        </span>
+      </p>
+      <p>
+        <br>FOR: Peter Stercx
+        <br>AGAINST: Humzah Kiani
+      </p>
+    </div>
+    <!-- BET DETAILS -->
+    
 </template>
 
 <style scoped>
